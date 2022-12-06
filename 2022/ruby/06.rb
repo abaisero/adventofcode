8296fdfc04312562e224ca1d9a2d6bcee55e352e
abaisero <andrea.baisero@gmail.com>
2022-12-06 00:15:25 -0500

    add 2022 06.rb

---
+++ 2022/ruby/06.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'stringio'
+require_relative 'test'
+
+def read_data(io)
+  io.read.strip
+end
+
+def find_first_uniq_index(signal, length)
+  signal.each_char.each_cons(length).find_index { |chars| chars.length == chars.uniq.length } + length
+end
+
+def part1(io)
+  signal = read_data io
+  find_first_uniq_index signal, 4
+end
+
+def part2(io)
+  signal = read_data io
+  find_first_uniq_index signal, 14
+end
+
+example = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
+test_example StringIO.open(example) { |io| part1 io }, 7
+test_example StringIO.open(example) { |io| part2 io }, 19
+
+example = 'bvwbjplbgvbhsrlpgdmjqwftvncz'
+test_example StringIO.open(example) { |io| part1 io }, 5
+test_example StringIO.open(example) { |io| part2 io }, 23
+
+example = 'nppdvjthqldpwncqszvftbrmjlhg'
+test_example StringIO.open(example) { |io| part1 io }, 6
+test_example StringIO.open(example) { |io| part2 io }, 23
+
+example = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
+test_example StringIO.open(example) { |io| part1 io }, 10
+test_example StringIO.open(example) { |io| part2 io }, 29
+
+example = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
+test_example StringIO.open(example) { |io| part1 io }, 11
+test_example StringIO.open(example) { |io| part2 io }, 26
+
+input = "#{File.basename(__FILE__, '.rb')}.txt"
+puts File.open(input) { |io| part1 io }
+puts File.open(input) { |io| part2 io }
